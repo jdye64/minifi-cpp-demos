e21e7fa903f33c62858851e3a358bc221dcf0557
Jeremy Dyer <jeremydyer@apache.org>
2018-07-23 22:58:04 -0400

    some testing

--- bootstrap-demo.sh
+++ bootstrap-demo.sh
@@ -6,11 +6,6 @@ GIT_DEMO_REPO=https://github.com/jdye64/$REPO_NAME.git
 DEMO_HOME=$PWD/$REPO_NAME
 MINIFI_HOME=/opt/nifi-minifi-cpp-0.6.0
 
-# Ensure that the MINIFI_HOME exists
-#if [ ! -d "$MINIFI_HOME" ]; then
-#  echo "The MiNiFi Home directory of: $MINIFI_HOME does not exist on this machine"; exit 1;
-#fi
-
 # Ensuring the required programs are installed
 command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed. Aborting."; exit 1; }
 
@@ -37,6 +32,11 @@ done
 # Listen for the demo that you want to install
 read -p "Which Demo do you want to install?: " demoName
 echo "Deploying Demo: '$demoName' to '$MINIFI_HOME'"
+
+# Ensure that the MINIFI_HOME exists
+if [ ! -d "$MINIFI_HOME" ]; then
+  echo "The MiNiFi Home directory of: $MINIFI_HOME does not exist on this machine"; exit 1;
+fi
 
 # Copy the configurations to MINIFI_HOME
 cp $PWD/Demos/$demoName/* $MINIFI_HOME/conf/.
